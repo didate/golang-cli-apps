43cecb2731c8d4abe002072bb28a9124d53b701e
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2022-09-28 22:34:49 +0000

    replace using os.Args by flag

--- interracting/todo/cmd/todo/main.go
+++ interracting/todo/cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/didate/interracting/todo"
 )
@@ -11,6 +11,13 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+
+	task := flag.String("task","","Task to be included in the ToDo List")
+	list := flag.Bool("list",false,"List of all tasks")
+	complete := flag.Int("complete",0,"Item to be completed")
+	
+	flag.Parse()
+
 	l := &todo.List{}
 
 	if err := l.Get(todoFileName); err != nil {
@@ -19,17 +26,27 @@ func main() {
 	}
 
 	switch {
-	case len(os.Args) == 1:
+	case *list:
 		for _, item := range *l {
 			fmt.Println(item.Task)
 		}
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
+	case *complete >0:
+		if err:= l.Complete(*complete); err!=nil{
+			fmt.Fprintln(os.Stderr, err)
+		}
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *task != "":
+		l.Add(*task)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 
 }
